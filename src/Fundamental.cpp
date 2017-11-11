7b8bb5fe47700f75ec652e37303e0d34fe549fbd
Andrew Belt <andrewpbelt@gmail.com>
2017-11-11 01:58:49 -0500

    Update createModel()

--- src/Fundamental.cpp
+++ src/Fundamental.cpp
@@ -9,18 +9,19 @@ void init(rack::Plugin *p) {
 #ifdef VERSION
 	p->version = TOSTRING(VERSION);
 #endif
-	p->addModel(createModel<VCOWidget>("Fundamental", "Fundamental", "VCO", "VCO-1"));
-	p->addModel(createModel<VCO2Widget>("Fundamental", "Fundamental", "VCO2", "VCO-2"));
-	p->addModel(createModel<VCFWidget>("Fundamental", "Fundamental", "VCF", "VCF"));
-	p->addModel(createModel<VCAWidget>("Fundamental", "Fundamental", "VCA", "VCA"));
-	p->addModel(createModel<LFOWidget>("Fundamental", "Fundamental", "LFO", "LFO-1"));
-	p->addModel(createModel<LFO2Widget>("Fundamental", "Fundamental", "LFO2", "LFO-2"));
-	p->addModel(createModel<DelayWidget>("Fundamental", "Fundamental", "Delay", "Delay"));
-	p->addModel(createModel<ADSRWidget>("Fundamental", "Fundamental", "ADSR", "ADSR"));
-	p->addModel(createModel<VCMixerWidget>("Fundamental", "Fundamental", "VCMixer", "VC Mixer"));
-	p->addModel(createModel<_8vertWidget>("Fundamental", "Fundamental", "8vert", "8vert"));
-	p->addModel(createModel<UnityWidget>("Fundamental", "Fundamental", "Unity", "Unity"));
-	p->addModel(createModel<MutesWidget>("Fundamental", "Fundamental", "Mutes", "Mutes"));
-	p->addModel(createModel<ScopeWidget>("Fundamental", "Fundamental", "Scope", "Scope"));
-	p->addModel(createModel<SEQ3Widget>("Fundamental", "Fundamental", "SEQ3", "SEQ-3"));
+
+	p->addModel(createModel<VCOWidget>("Fundamental", "VCO", "VCO-1"));
+	p->addModel(createModel<VCO2Widget>("Fundamental", "VCO2", "VCO-2"));
+	p->addModel(createModel<VCFWidget>("Fundamental", "VCF", "VCF"));
+	p->addModel(createModel<VCAWidget>("Fundamental", "VCA", "VCA"));
+	p->addModel(createModel<LFOWidget>("Fundamental", "LFO", "LFO-1"));
+	p->addModel(createModel<LFO2Widget>("Fundamental", "LFO2", "LFO-2"));
+	p->addModel(createModel<DelayWidget>("Fundamental", "Delay", "Delay"));
+	p->addModel(createModel<ADSRWidget>("Fundamental", "ADSR", "ADSR"));
+	p->addModel(createModel<VCMixerWidget>("Fundamental", "VCMixer", "VC Mixer"));
+	p->addModel(createModel<_8vertWidget>("Fundamental", "8vert", "8vert"));
+	p->addModel(createModel<UnityWidget>("Fundamental", "Unity", "Unity"));
+	p->addModel(createModel<MutesWidget>("Fundamental", "Mutes", "Mutes"));
+	p->addModel(createModel<ScopeWidget>("Fundamental", "Scope", "Scope"));
+	p->addModel(createModel<SEQ3Widget>("Fundamental", "SEQ3", "SEQ-3"));
 }
